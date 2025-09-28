21f85b42e5c97fc2575ec641614d3df52463fbb0
JMGamma <jaymgamma@gmail.com>
2025-09-27 22:05:09 -0600

    Ship created

--- main.py
+++ main.py
@@ -1,18 +1,27 @@
-import pygame, constants
+import pygame, constants, player
 
 
 def main():
     pygame.init
     screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+    ship = player.Player((constants.SCREEN_WIDTH / 2), (constants.SCREEN_HEIGHT / 2))
+
+
+
     while True == True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        
-        pygame.Surface.fill(screen, ("black"))
-        pygame.display.flip()
-    print(f"Starting Asteroids!\nScreen width: {constants.SCREEN_WIDTH}\nScreen height: {constants.SCREEN_HEIGHT}")
 
+        pygame.Surface.fill(screen, ("black"))
+        ship.draw(screen)
+        pygame.display.flip()
+
+        dt = clock.tick(60) /1000
+        
+    
 
 if __name__ == "__main__":
     main()
